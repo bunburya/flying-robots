bcd138b49491016a6ccd93827503a8609e474b0b
bunburya <bunburya@tcd.ie>
2012-03-05 04:10:11 +0000

    using argparse on ver >= 2.3

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-from optparse import OptionParser
+from sys import version_info
+if version_info.minor < 2:
+    from optparse import OptionParser as ArgumentParser
+else:
+    from argparse import ArgumentParser
 
 import ui
 from config import get_config
 
-parser = OptionParser()
-parser.add_option('-c', '--config', dest='conf_file',
+parser = ArgumentParser()
+parser.add_argument('-c', '--config', dest='conf_file',
                   help='provide a custom configuration file', metavar='FILE')
-parser.add_option('-l', '--level', dest='start_level',
+parser.add_argument('-l', '--level', dest='start_level',
                   help='start playing at the specified level', metavar='LEVEL')
-parser.add_option('-n', '--name', dest='name',
+parser.add_argument('-n', '--name', dest='name',
                   help='specify player name', metavar='NAME')
 
-options, args = parser.parse_args()
+options = parser.parse_args()
 conf = get_config(options)
 
 ui.GameInterface(conf)
