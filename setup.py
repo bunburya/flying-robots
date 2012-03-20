87e2651bed2e6c07ee79b3b364f389c0fb624638
bunburya <bunburya@tcd.ie>
2012-03-20 02:08:40 +0000

    removed long description

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 from distutils.core import setup
 
-with open('USAGE', 'r') as f:
-    long_desc = f.read()
-
 setup(
         name='flybots',
         version='0.1.0',
@@ -11,7 +8,6 @@ setup(
         scripts=['flybots'],
         url='https://github.com/bunburya/FlyingRobots',
         description='A 3D clone of the classic bsd-robots game.',
-        long_description=long_desc,
         download_url='https://github.com/bunburya/FlyingRobots',
         platforms=['unix', 'posix', 'linux', 'bsd'],
         license='MIT'
